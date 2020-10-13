d90d54f8c347267256dd8dd409fe49c90f1e5138
hma <hhh>
2020-10-13 10:27:04 +0800

    ok

--- st/pmgt/indicator/dao/ExpectedFinishTimeDao.java
+++
@@ -1,8 +0,0 @@
-package com.st.pmgt.indicator.dao;
-
-import com.pisx.tundra.foundation.fc.model.ObjectIdentifier;
-import com.st.pmgt.indicator.model.STExpectedFinishTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpectedFinishTimeDao extends JpaRepository<STExpectedFinishTime, ObjectIdentifier> {
-}
--- st/pmgt/indicator/dao/ProCompetenceDao.java
+++
@@ -1,8 +0,0 @@
-package com.st.pmgt.indicator.dao;
-
-import com.pisx.tundra.foundation.fc.model.ObjectIdentifier;
-import com.st.pmgt.indicator.model.STProCompetence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProCompetenceDao extends JpaRepository<STProCompetence, ObjectIdentifier> {
-}
