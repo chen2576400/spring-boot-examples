67d14359b2e4367474a84dee03dd9cf6b309e98d
nchen <you>
2020-11-20 14:26:51 +0800

    导入

--- src/main/java/ext/st/pmgt/indicator/dao/DeliverableTypeDao.java
+++ src/main/java/ext/st/pmgt/indicator/dao/DeliverableTypeDao.java
@@ -4,6 +4,8 @@ import com.pisx.tundra.foundation.fc.model.ObjectIdentifier;
 import ext.st.pmgt.indicator.model.STDeliverableType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 /**
  * @ClassName DeliverableTypeDao
@@ -15,4 +17,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DeliverableTypeDao extends JpaRepository<STDeliverableType, ObjectIdentifier> {
     STDeliverableType findByName(String s);
     STDeliverableType findByNameEquals(String s);
+    List<STDeliverableType> findByCodeEquals(String S);
+
 }
