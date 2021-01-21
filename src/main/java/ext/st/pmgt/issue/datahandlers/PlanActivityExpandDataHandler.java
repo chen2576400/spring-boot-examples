113ca09d0246e0836ddfcc120a7667b7f989f824
nchen <2576400553@qq.com>
2021-01-21 13:41:18 +0800

    问题编辑获取活动

--- src/main/java/ext/st/pmgt/issue/datahandlers/PlanActivityExpandDataHandler.java
+++ src/main/java/ext/st/pmgt/issue/datahandlers/PlanActivityExpandDataHandler.java
@@ -1,5 +1,6 @@
 package ext.st.pmgt.issue.datahandlers;
 
+import com.pisx.tundra.foundation.fc.model.Persistable;
 import com.pisx.tundra.foundation.util.PIException;
 import com.pisx.tundra.netfactory.mvc.components.ComponentParams;
 import com.pisx.tundra.netfactory.mvc.components.div.DivElement;
@@ -8,11 +9,16 @@ import com.pisx.tundra.netfactory.mvc.components.input.InputElement;
 import com.pisx.tundra.netfactory.util.misc.URLFactory;
 import com.pisx.tundra.pmgt.change.datahandlers.PlanActivityDataHandler;
 import com.pisx.tundra.pmgt.plan.model.PIPlannable;
+import com.pisx.tundra.pmgt.project.model.PIProject;
 import ext.st.pmgt.issue.model.STProjectIssue;
+import ext.st.pmgt.issue.model.STProjectRisk;
 
 public class PlanActivityExpandDataHandler extends PlanActivityDataHandler {
     @Override
     public Object getDataValue(String columnName, Object datum, ComponentParams params) throws PIException {
+
+        Persistable sourceObject = params.getNfCommandBean().getSourceObject();
+
         InputElement inputElement = InputElement.instance(columnName);
         if (datum!=null){//编辑页面是需要回填到input框
             getInputValue(datum,inputElement);
@@ -32,7 +38,7 @@ public class PlanActivityExpandDataHandler extends PlanActivityDataHandler {
         });
 
         //context传给objpicker
-        String url = URLFactory.getActionHref("pi-pmgt-enterprise", "requestActivityPicker", params.getNfCommandBean().getSourceObject());
+        String url = URLFactory.getActionHref("pi-pmgt-enterprise", "requestActivityPicker",getContent(sourceObject));
 
         rightImg.backFill(url, columnName);
 
@@ -41,6 +47,16 @@ public class PlanActivityExpandDataHandler extends PlanActivityDataHandler {
         content.children(inputElement, rightImg);
         return content;
     }
+
+    private Object getContent(Persistable sourceObject){
+        if (sourceObject instanceof PIProject){
+            return (PIProject) sourceObject;
+        }else if (sourceObject instanceof STProjectIssue){
+            return ((STProjectIssue) sourceObject).getProject();
+        }
+        return null;
+    }
+
 
     public void getInputValue(Object datum, InputElement inputElement){
         super.getInputValue(datum,inputElement);
